f8f54c1d9a622915784b459c682e13cc6cfc961c
Vagner Martins <va_martins3@hotmail.com>
2024-10-05 14:39:21 -0300

    GET item detail em ItemDetailActivity

--- app/src/main/java/com/example/minhaprimeiraapi/ItemDetailActivity.kt
+++ app/src/main/java/com/example/minhaprimeiraapi/ItemDetailActivity.kt
@@ -2,18 +2,30 @@ package com.example.minhaprimeiraapi
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.minhaprimeiraapi.databinding.ActivityItemDetailBinding
+import com.example.minhaprimeiraapi.model.Item
+import com.example.minhaprimeiraapi.service.Result
+import com.example.minhaprimeiraapi.service.RetrofitClient
+import com.example.minhaprimeiraapi.service.safeApiCall
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ItemDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityItemDetailBinding
+
+    private lateinit var item: Item
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityItemDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
+        loadItem()
     }
 
     private fun setupView() {
@@ -23,6 +35,30 @@ class ItemDetailActivity : AppCompatActivity() {
         binding.toolbar.setNavigationOnClickListener {
             finish()
         }
+    }
+
+    private fun loadItem() {
+        val itemId = intent.getStringExtra(ARG_ID) ?: ""
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = safeApiCall { RetrofitClient.apiService.getItem(itemId) }
+
+            withContext(Dispatchers.Main) {
+                when (result) {
+                    is Result.Error -> {}
+                    is Result.Success -> {
+                        item = result.data
+                        handleSuccess()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun handleSuccess() {
+        binding.name.text = "${item.value.name} ${item.value.surname}"
+        binding.age.text = getString(R.string.item_age, item.value.age.toString())
+        binding.address.text = item.value.address
     }
 
     companion object {
--- app/src/main/java/com/example/minhaprimeiraapi/service/ApiService.kt
+++ app/src/main/java/com/example/minhaprimeiraapi/service/ApiService.kt
@@ -2,9 +2,13 @@ package com.example.minhaprimeiraapi.service
 
 import com.example.minhaprimeiraapi.model.Item
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("items")
     suspend fun getItems(): List<Item>
+
+    @GET("items/{id}")
+    suspend fun getItem(@Path("id") id: String): Item
 }

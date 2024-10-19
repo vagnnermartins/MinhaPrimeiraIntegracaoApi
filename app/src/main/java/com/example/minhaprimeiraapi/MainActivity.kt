2a800ea12af3b596047e9bbd61c0cae220fe486e
Vagner Martins <va_martins3@hotmail.com>
2024-10-19 10:31:09 -0300

    Permissão API / Location API
    Adicionando pedido de permissão de localização
    Obtendo ultima localização

--- app/src/main/java/com/example/minhaprimeiraapi/MainActivity.kt
+++ app/src/main/java/com/example/minhaprimeiraapi/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.example.minhaprimeiraapi
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.location.Location
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat.checkSelfPermission
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.minhaprimeiraapi.adapter.ItemAdapter
 import com.example.minhaprimeiraapi.databinding.ActivityMainBinding
@@ -9,6 +19,9 @@ import com.example.minhaprimeiraapi.model.Item
 import com.example.minhaprimeiraapi.service.Result
 import com.example.minhaprimeiraapi.service.RetrofitClient
 import com.example.minhaprimeiraapi.service.safeApiCall
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,17 +30,69 @@ import kotlinx.coroutines.withContext
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationPermissionLauncher: ActivityResultLauncher<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requestLocationPermission()
         setupView()
     }
 
     override fun onResume() {
         super.onResume()
         fetchItems()
+    }
+
+    private fun requestLocationPermission() {
+        // Inicializa o FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        // Configura o ActivityResultLauncher para solicitar a permissão de localização
+        locationPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                // Permissão concedida, obter a localização
+                getLastLocation()
+            } else {
+                Toast.makeText(this, "Permissão de localização negada", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        checkLocationPermissionAndRequest()
+    }
+
+    private fun checkLocationPermissionAndRequest() {
+        when {
+            checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED -> {
+                getLastLocation()
+            }
+            shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION) -> {
+                locationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+            }
+            shouldShowRequestPermissionRationale(ACCESS_COARSE_LOCATION) -> {
+                locationPermissionLauncher.launch(ACCESS_COARSE_LOCATION)
+            }
+            else -> {
+                locationPermissionLauncher.launch(ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    private fun getLastLocation() {
+        if (checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED ||
+            checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PERMISSION_GRANTED) {
+            requestLocationPermission()
+            return
+        }
+        fusedLocationClient.lastLocation.addOnCompleteListener { task: Task<Location> ->
+            if (task.isSuccessful && task.result != null) {
+                val location = task.result
+
+            } else {
+                Toast.makeText(this, R.string.unknown_error, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun fetchItems() {

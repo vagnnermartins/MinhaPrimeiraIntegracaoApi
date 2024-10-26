9b151c0ba95d7f7d7e19f436555bc0cae4fb14d2
Vagner Martins <va_martins3@hotmail.com>
2024-10-26 12:08:02 -0300

    Login com PhoneNumber do Firebase

--- app/src/main/java/com/example/minhaprimeiraapi/LoginActivity.kt
+++ app/src/main/java/com/example/minhaprimeiraapi/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.minhaprimeiraapi
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
@@ -12,8 +13,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class LoginActivity : AppCompatActivity() {
 
@@ -21,6 +29,8 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInLauncher: ActivityResultLauncher<Intent>
+
+    private var verificationId = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,26 +65,84 @@ class LoginActivity : AppCompatActivity() {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
             .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    Log.d("LoginActivity", "User: ${user?.uid}")
-                    startActivity(
-                        MainActivity.newIntent(this)
-                    )
-                } else {
-                    Toast.makeText(
-                        this,
-                        "Erro com o Login usando o Google",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+                onCredentialCompleteListener(task, "Google")
             }
+    }
+
+    private fun onCredentialCompleteListener(
+        task: Task<AuthResult>,
+        loginType: String
+    ) {
+        if (task.isSuccessful) {
+            val user = auth.currentUser
+            Log.d("LoginActivity", "LoginType: $loginType User: ${user?.uid}")
+            startActivity(
+                MainActivity.newIntent(this)
+            )
+        } else {
+            Toast.makeText(
+                this,
+                "${task.exception?.localizedMessage}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 
     private fun setupView() {
         binding.googleSignInButton.setOnClickListener {
             signIn()
         }
+        binding.btnSendSms.setOnClickListener {
+            sendVerificationCode()
+        }
+        binding.btnVerifySms.setOnClickListener {
+            verifyCode()
+        }
+    }
+
+    private fun sendVerificationCode() {
+        val phoneNumber = binding.cellphone.text.toString()
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+
+                }
+
+                override fun onVerificationFailed(e: FirebaseException) {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "${e.message}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onCodeSent(verificationId: String, token: PhoneAuthProvider.ForceResendingToken) {
+                    this@LoginActivity.verificationId = verificationId
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Código de verificação enviado",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    binding.btnVerifySms.visibility = View.VISIBLE
+                    binding.veryfyCode.visibility = View.VISIBLE
+                }
+
+            })
+            .build()
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    private fun verifyCode() {
+        val verificationCode = binding.veryfyCode.text.toString()
+        val credential = PhoneAuthProvider.getCredential(verificationId, verificationCode)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                onCredentialCompleteListener(task, "Phone Number")
+            }
     }
 
     private fun signIn() {
